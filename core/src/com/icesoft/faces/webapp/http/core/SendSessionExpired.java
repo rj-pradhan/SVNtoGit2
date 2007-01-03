6815416d61a907d2c2b409f77a903ed1c5f35c23
mircea <mircea>
2007-01-03 00:00:06 +0000

    Call SessionLifetimeManager.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces@12722 8668f098-c06c-11db-ba21-f49e70c34f74

--- core/src/com/icesoft/faces/webapp/http/core/SendSessionExpired.java
+++ core/src/com/icesoft/faces/webapp/http/core/SendSessionExpired.java
@@ -5,12 +5,15 @@ import com.icesoft.faces.webapp.http.Request;
 import com.icesoft.faces.webapp.http.ResponseHandler;
 import com.icesoft.faces.webapp.http.Response;
 import com.icesoft.faces.webapp.xmlhttp.SessionExpiredException;
+import com.icesoft.faces.webapp.xmlhttp.SessionLifetimeManager;
 
+import javax.servlet.http.HttpSession;
 import java.io.StringWriter;
 import java.io.ByteArrayInputStream;
 
 public class SendSessionExpired implements Server {
     private Server server;
+    private HttpSession session;
     private final static ResponseHandler Handler = new ResponseHandler() {
 
         public void respond(Response response) throws Exception {
@@ -20,12 +23,17 @@ public class SendSessionExpired implements Server {
         }
     };
 
-    public SendSessionExpired(Server server) {
+    public SendSessionExpired(HttpSession session, Server server) {
         this.server = server;
+        this.session = session;
     }
 
     public void service(Request request) throws Exception {
         try {
+            if (!request.getURI().getPath().endsWith("ping")) {
+                //todo: move SessionLifetimeManager's implementation in this class 
+                SessionLifetimeManager.touch(session);
+            }
             server.service(request);
         } catch (SessionExpiredException e) {
             request.respondWith(Handler);
