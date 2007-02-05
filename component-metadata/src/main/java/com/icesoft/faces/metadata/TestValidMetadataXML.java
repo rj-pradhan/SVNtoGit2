2994ffe0072e3121ec51311fab2863e461bb2501
frank <frank>
2007-02-05 17:02:09 +0000

    #1309 error handler report entity location etc ...
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces@13122 8668f098-c06c-11db-ba21-f49e70c34f74

--- component-metadata/src/main/java/com/icesoft/faces/metadata/TestValidMetadataXML.java
+++ component-metadata/src/main/java/com/icesoft/faces/metadata/TestValidMetadataXML.java
@@ -30,189 +30,210 @@ import com.sun.rave.jsfmeta.beans.RenderKitBean;
 import com.sun.rave.jsfmeta.beans.RendererBean;
 
 public class TestValidMetadataXML{
-
-
-	public static void main(String[] args) {
-
-		TestValidMetadataXML test = new TestValidMetadataXML();
-		test.setUp();
-		test.testMetadata();
-		test.testMetadataBeans();
-	}
-
-
-	private File getConfDir() {
-		ClassLoader classLoader = Thread.currentThread()
-				.getContextClassLoader();
-		URL url = classLoader.getResource(".");
-
-		File buildDir = new File( convertFileUrlToPath(url) );
-
-		if(!buildDir.isDirectory()){
-			System.out.println("test build directory does not exist: "+buildDir);
-		}
-
-		File confFile = new File(buildDir.getParent() + File.separatorChar
-				+ "classes" + File.separator + "conf");
-
-		return confFile;
-	}
-
-	protected void setUp() {
-
-		File confFile = getConfDir();
-
-		boolean isConfFile = confFile.isDirectory();
-		if(!isConfFile){
-			System.out.println("no conf directory in the build directory: "+ confFile);
-			if( !confFile.mkdirs() )
-				System.out.println("conf directory could not be created");
-		}
-
-		String xsltStreamSourceString = confFile.getPath() + File.separatorChar
-				+ "xslt_conf" + File.separatorChar + "translate-conf.xsl";
-		String outputStreamString = confFile.getPath() + File.separatorChar
-				+ "sun-faces-config-verified.xml";
-		String streamSourceString = confFile.getPath() + File.separatorChar
-				+ "sun-faces-config.xml";
-
-		try {
-
-			TransformerFactory tFactory = TransformerFactory.newInstance();
-			StreamSource xsltStreamSource = new StreamSource(
-					xsltStreamSourceString);
-			Transformer transformer = tFactory.newTransformer(xsltStreamSource);
-			OutputStream outputStream = new FileOutputStream(outputStreamString);
-			StreamResult streamResult = new StreamResult(outputStream);
-			StreamSource streamSource = new StreamSource(streamSourceString);
-			transformer.transform(streamSource, streamResult);
-
-		} catch (TransformerException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void testMetadata() {
-
-		File confDir = getConfDir();
-
-		String outputStreamString = confDir.getPath() + File.separatorChar
-				+ "sun-faces-config.xml";
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-				.newInstance();
-		documentBuilderFactory.setValidating(true);
-		documentBuilderFactory.setNamespaceAware(true);
-
-		DocumentBuilder documentBuilder = null;
-		try {
-			documentBuilder = documentBuilderFactory.newDocumentBuilder();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
-
-		documentBuilder.setErrorHandler(new ErrorHandler() {
-			public void error(SAXParseException e) {
-				e.printStackTrace();
-			}
-
-			public void fatalError(SAXParseException e) throws SAXException {
-				e.printStackTrace();
-			}
-
-			public void warning(SAXParseException e) {
-				e.printStackTrace();
-			}
-		});
-
-		try {
-			Document document = documentBuilder.parse(new File(
-					outputStreamString));
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void testMetadataBeans() {
-
-		PropertyBean[] pb = null;
-		String[] cb = null;
-		MetadataXmlParser metadataParser = new MetadataXmlParser();
-		metadataParser.setDesign(true);
-
-		try {
-			File confDir = getConfDir();
-
-			String newPath = confDir.getPath()+ File.separator			
-					+ "sun-faces-config-verified.xml";
-			File file = new File(newPath);
-
-			FacesConfigBean facesConfigBean = metadataParser.parse(file);
-
-			RenderKitBean renderKitBean = facesConfigBean
-					.getRenderKit(RenderKitFactory.HTML_BASIC_RENDER_KIT);
-			ComponentBean[] componentbeans = facesConfigBean.getComponents();
-
-			cb = new String[componentbeans.length];
-
-			for (int i = 0; i < componentbeans.length; i++) {
-				cb[i] = componentbeans[i].getComponentClass();
-				PropertyBean[] descriptions = componentbeans[i].getProperties();
-
-				componentbeans[i].getBaseComponentType();
-
-				RendererBean rendererBean = renderer(componentbeans[i],
-						renderKitBean);
-				String rendererType = rendererBean.getRendererType();
-				String renderClassName = rendererBean.getRendererClass();
-
-				String one = "";
-				pb = descriptions;
-				for (int j = 0; j < pb.length; j++) {
-					one = one + "\n" + pb[j].getPropertyName();
-				}
-
-			}
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	protected RendererBean renderer(ComponentBean cb,
-			RenderKitBean renderKitBean) {
-		String rendererType = cb.getRendererType();
-		if (rendererType == null)
-			return null;
-		String componentFamily = cb.getComponentFamily();
-		if (componentFamily == null) {
-			System.out.println("TODO: component family is null");
-		}
-		RendererBean rb = renderKitBean.getRenderer(componentFamily,
-				rendererType);
-
-		if (rb == null) {
-			System.out.println("TODO: RenderBean componentFamily=" + componentFamily
-					+ " rendererType=" + rendererType);
-		}
-
-		if (rb == null) {
-			System.out.println("TODO: no renderer bean");
-		}
-		return rb;
-	}
-
+    
+    
+    public static void main(String[] args) {
+        
+        TestValidMetadataXML test = new TestValidMetadataXML();
+        test.setUp();
+        test.testMetadata();
+        test.testMetadataBeans();
+        
+    }
+    
+    
+    private File getConfDir() {
+        ClassLoader classLoader = Thread.currentThread()
+        .getContextClassLoader();
+        URL url = classLoader.getResource(".");
+        
+        File buildDir = new File( convertFileUrlToPath(url) );
+        
+        if(!buildDir.isDirectory()){
+            System.out.println("test build directory does not exist: "+buildDir);
+        }
+        
+        File confFile = new File(buildDir.getParent() + File.separatorChar
+                + "classes" + File.separator + "conf");
+        
+        return confFile;
+    }
+    
+    protected void setUp(){
+        
+        File confFile = getConfDir();
+        
+        boolean isConfFile = confFile.isDirectory();
+        if(!isConfFile){
+            System.out.println("no conf directory in the build directory: "+ confFile);
+            if( !confFile.mkdirs() )
+                System.out.println("conf directory could not be created");
+        }
+        
+        String xsltStreamSourceString = confFile.getPath() + File.separatorChar
+                + "xslt_conf" + File.separatorChar + "translate-conf.xsl";
+        String outputStreamString = confFile.getPath() + File.separatorChar
+                + "sun-faces-config-verified.xml";
+        String streamSourceString = confFile.getPath() + File.separatorChar
+                + "sun-faces-config.xml";
+        
+        try {
+            
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            StreamSource xsltStreamSource = new StreamSource(
+                    xsltStreamSourceString);
+            Transformer transformer = tFactory.newTransformer(xsltStreamSource);
+            OutputStream outputStream = new FileOutputStream(outputStreamString);
+            StreamResult streamResult = new StreamResult(outputStream);
+            StreamSource streamSource = new StreamSource(streamSourceString);
+            transformer.transform(streamSource, streamResult);
+            
+        } catch (TransformerException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    
+    public void testMetadata(){
+        
+        File confDir = getConfDir();
+        
+        String outputStreamString = confDir.getPath() + File.separatorChar
+                + "sun-faces-config.xml";
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+                .newInstance();
+        documentBuilderFactory.setValidating(true);
+        documentBuilderFactory.setNamespaceAware(true);
+        
+        DocumentBuilder documentBuilder = null;
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+        documentBuilder.setErrorHandler(new ErrorHandler() {
+            public void error(SAXParseException ex) {
+                System.err.println("Please check the following \n"+"line number="+ex.getLineNumber()+
+                        " column number= "+ ex.getColumnNumber()+
+                        "\n URL="+ex.getSystemId());
+                ex.printStackTrace();
+                System.exit(1);
+            }
+            
+            public void fatalError(SAXParseException ex) throws SAXException {
+                System.err.println("Please check the following \n"+"line number="+ex.getLineNumber()+
+                        " column number="+ ex.getColumnNumber()+
+                        "\n URL="+ex.getSystemId());
+                ex.printStackTrace();
+                System.exit(1);
+            }
+            
+            public void warning(SAXParseException ex) {
+                System.err.println("Please check the following \n"+"line number="+ex.getLineNumber()+
+                        " column number="+ ex.getColumnNumber()+
+                        "\n URL="+ex.getSystemId());
+                ex.printStackTrace();
+                System.exit(1);
+            }
+        });
+        
+        try {
+            Document document = documentBuilder.parse(new File(outputStreamString));
+        }  catch (SAXException se) {
+            se.printStackTrace();
+            System.exit(1);
+        }  catch (IOException se) {
+            se.printStackTrace();
+            System.exit(1);
+        }
+    }
+    
+    public void testMetadataBeans(){
+        
+        PropertyBean[] pb = null;
+        String[] cb = null;
+        MetadataXmlParser metadataParser = new MetadataXmlParser();
+        metadataParser.setDesign(true);
+        
+        try {
+            File confDir = getConfDir();
+            
+            String newPath = confDir.getPath()+ File.separator
+                    + "sun-faces-config-verified.xml";
+            File file = new File(newPath);
+            
+            FacesConfigBean facesConfigBean = metadataParser.parse(file);
+            
+            RenderKitBean renderKitBean = facesConfigBean
+                    .getRenderKit(RenderKitFactory.HTML_BASIC_RENDER_KIT);
+            ComponentBean[] componentbeans = facesConfigBean.getComponents();
+            
+            cb = new String[componentbeans.length];
+            
+            for (int i = 0; i < componentbeans.length; i++) {
+                cb[i] = componentbeans[i].getComponentClass();
+                PropertyBean[] descriptions = componentbeans[i].getProperties();
+                
+                componentbeans[i].getBaseComponentType();
+                
+                RendererBean rendererBean = renderer(componentbeans[i],
+                        renderKitBean);
+                String rendererType = rendererBean.getRendererType();
+                String renderClassName = rendererBean.getRendererClass();
+                
+                String one = "";
+                pb = descriptions;
+                for (int j = 0; j < pb.length; j++) {
+                    one = one + "\n" + pb[j].getPropertyName();
+                }
+                
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (SAXException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    
+    protected RendererBean renderer(ComponentBean cb,
+            RenderKitBean renderKitBean) {
+        String rendererType = cb.getRendererType();
+        if (rendererType == null)
+            return null;
+        String componentFamily = cb.getComponentFamily();
+        if (componentFamily == null) {
+            System.out.println("TODO: component family is null");
+        }
+        RendererBean rb = renderKitBean.getRenderer(componentFamily,
+                rendererType);
+        
+        if (rb == null) {
+            System.out.println("TODO: RenderBean componentFamily=" + componentFamily
+                    + " rendererType=" + rendererType);
+        }
+        
+        if (rb == null) {
+            System.out.println("TODO: no renderer bean");
+        }
+        return rb;
+    }
+    
     /**
      * Kind of hack-ish attempt at solving problem that if the directory,
      *  where we're building the component-metadata in,  has special
@@ -234,7 +255,7 @@ public class TestValidMetadataXML{
                             increment++;
                         try {
                             char unescaped = (char) Integer.parseInt(
-                                path.substring(i+1, i+increment), 16);
+                                    path.substring(i+1, i+increment), 16);
                             
                             sb.append( unescaped );
                             i += increment;
