004bca50d5cdffe494b9dd7bcd7da1146bf0abf4
philip.breau <philip.breau>
2007-03-10 23:25:48 +0000

    ICE-1322 checking for null tld to allow compatibility with struts jars
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk@13381 8668f098-c06c-11db-ba21-f49e70c34f74

--- icefaces/core/src/com/icesoft/jasper/compiler/TldLocationsCache.java
+++ icefaces/core/src/com/icesoft/jasper/compiler/TldLocationsCache.java
@@ -445,12 +445,14 @@ public class TldLocationsCache {
             throws JasperException {
         // Parse the tag library descriptor at the specified resource path
         TreeNode tld = new ParserUtils().parseXMLDocument(resourcePath, in);
-        TreeNode uri = tld.findChild("uri");
-        if (uri != null) {
-            String body = uri.getBody();
-            if (body != null)
-                return body;
-        }
+        if( tld != null ){
+        	TreeNode uri = tld.findChild("uri");
+            if (uri != null) {
+                String body = uri.getBody();
+                if (body != null)
+                    return body;
+            }
+        }        
 
         return null;
     }
