04f1db95eeebe18495f40009a06187c45b5e90bf
carlo.guglielmin <carlo.guglielmin>
2007-03-19 16:33:24 +0000

    Removed built files
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk@13397 8668f098-c06c-11db-ba21-f49e70c34f74

--- icefaces/bridge/openajaxconformancetests/web/WEB-INF/classes/com/icesoft/openajax/beans/UserBean.java
+++
@@ -1,9 +0,0 @@
-package com.icesoft.openajax.beans;
-
-public class UserBean
-{
-    private String username = "Carlo";
-    
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-}
