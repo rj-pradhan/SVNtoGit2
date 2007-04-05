bad5ac0be474a44669e5e2f381e99596d04183c2
mircea toma <mircea.toma@gmail.com>
2007-04-05 21:55:59 +0000

    Introduce separate ExternalContext.requestMap implementation. Make ExternalContext.requestMap behavior configurable -- ICE-1413.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk@13473 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ icefaces/core/src/com/icesoft/faces/webapp/http/servlet/DirectRequestAttributeMap.java
@@ -0,0 +1,30 @@
+package com.icesoft.faces.webapp.http.servlet;
+
+import com.icesoft.faces.context.AbstractAttributeMap;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+public class DirectRequestAttributeMap extends AbstractAttributeMap {
+    private HttpServletRequest request;
+
+    public DirectRequestAttributeMap(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    protected Object getAttribute(String key) {
+        return request.getAttribute(key);
+    }
+
+    protected void setAttribute(String key, Object value) {
+        request.setAttribute(key, value);
+    }
+
+    protected void removeAttribute(String key) {
+        request.removeAttribute(key);
+    }
+
+    protected Enumeration getAttributeNames() {
+        return request.getAttributeNames();
+    }
+}
