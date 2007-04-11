aa8b1583f633ce77fc8be0020652c82c07ced1b6
rob.mayhew <rob.mayhew>
2007-04-11 20:03:42 +0000

    Fix for ICE-997. Clicking on IE now fires action events.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk/icefaces@13503 8668f098-c06c-11db-ba21-f49e70c34f74

--- component/src/com/icesoft/faces/component/selectinputtext/SelectInputText.java
+++ component/src/com/icesoft/faces/component/selectinputtext/SelectInputText.java
@@ -498,6 +498,9 @@ public class SelectInputText extends HtmlInputText implements NamingContainer {
             if("true".equals(requestParemeterMap.get("ice.event.left"))){
                 queueEvent(new ActionEvent(this));                
             }
+            if("onclick".equals(requestParemeterMap.get("ice.event.type"))){
+                queueEvent(new ActionEvent(this));
+            }
 
 
         } catch (Exception e) {
