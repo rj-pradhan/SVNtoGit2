20565759bf23d4b190b9ca00b32fe343441f5760
ted.goddard <ted.goddard>
2007-04-18 18:28:19 +0000

    invoking action directly rather than using broadcast as a JSF execution context may not be present ICE-1448
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk/icefaces@13578 8668f098-c06c-11db-ba21-f49e70c34f74

--- component/src/com/icesoft/faces/component/inputfile/InputFile.java
+++ component/src/com/icesoft/faces/component/inputfile/InputFile.java
@@ -140,7 +140,8 @@ public class InputFile extends UICommand implements Serializable, FileUploadComp
                 Streams.copy(stream.openStream(), output, true);
                 status = SAVED;
                 fileInfo.setPhysicalPath(file.getAbsolutePath());
-                broadcast(new ActionEvent(this));
+                getActionListener().invoke( FacesContext.getCurrentInstance(), 
+                        new Object[] {new ActionEvent(this)} );
             } else {
                 status = INVALID_NAME_PATTERN;
                 context.addMessage(null, MessageUtils.getMessage(context, INVALID_NAME_PATTERN_MESSAGE_ID, new Object[] { fileName, namePattern }));
