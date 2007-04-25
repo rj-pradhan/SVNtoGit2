7ab23b07a04789d9d3d0719c1f21a4bf7ea9265f
rob.mayhew <rob.mayhew>
2007-04-25 22:50:09 +0000

    ICE-1177, Accordion Panel changes
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk/icefaces@13646 8668f098-c06c-11db-ba21-f49e70c34f74

--- component/src/com/icesoft/faces/component/accordion/PanelAccordionRenderer.java
+++ component/src/com/icesoft/faces/component/accordion/PanelAccordionRenderer.java
@@ -2,8 +2,10 @@ package com.icesoft.faces.component.accordion;
 
 import com.icesoft.faces.context.DOMContext;
 import com.icesoft.faces.context.effects.CurrentStyle;
+import com.icesoft.faces.context.effects.JavascriptContext;
 import com.icesoft.faces.renderkit.dom_html_basic.DomBasicRenderer;
 import com.icesoft.faces.renderkit.dom_html_basic.HTML;
+import com.icesoft.faces.component.util.CustomComponentUtils;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -32,24 +34,30 @@ public class PanelAccordionRenderer extends DomBasicRenderer {
 
     public void decode(FacesContext context, UIComponent component) {
         super.decode(context, component);
+        PanelAccordionState state = PanelAccordionState.getState(context, component);
+        state.setChangedViaDecode(false);
         try {
+            PanelAccordion panelAccordion = (PanelAccordion) component;
+
             Map map = (Map) context.getExternalContext().getSessionMap()
                     .get(CurrentStyle.class.getName());
             if (map != null) {
                 String baseId = component.getClientId(context);
                 String contentId = baseId + "_content";
                 String style = (String) map.get(contentId);
-                PanelAccordion panelAccordion = (PanelAccordion) component;
+
                 if (style != null) {
                     Boolean newState = Boolean.TRUE;
                     if (style.indexOf("display:none") != -1) {
                         newState = Boolean.FALSE;
                     }
-                    Boolean currentState = panelAccordion.getOpen();
+                    Boolean currentState = panelAccordion.getExpanded();
                     if (!newState.equals(currentState)) {
                         ActionEvent ae = new ActionEvent(component);
                         component.queueEvent(ae);
-                        panelAccordion.setOpen(newState);
+                        panelAccordion.setExpanded(newState);
+
+                        state.setChangedViaDecode(true);
                     }
                 }
             }
@@ -61,11 +69,35 @@ public class PanelAccordionRenderer extends DomBasicRenderer {
 
     public void encodeBegin(FacesContext facesContext, UIComponent uiComponent) throws IOException {
         PanelAccordion panelAccordion = (PanelAccordion) uiComponent;
+        PanelAccordionState state = PanelAccordionState.getState(facesContext, uiComponent);
         String base = panelAccordion.getStyleClass();
-        boolean open = panelAccordion.getOpen().booleanValue();
+        boolean open = panelAccordion.getExpanded().booleanValue();
+        boolean disabled = panelAccordion.isDisabled();
+
         String headerClass = base + HEADER;
-        String contentClass = base + CONTENT;
         String containerClass = base + CONTAINER;
+        String contentClass = base + CONTENT;
+        if(!open){
+            headerClass += "_collapsed";
+            contentClass += "_collapsed";                
+            containerClass += "_collapsed";
+        }
+        if(disabled){
+            headerClass += "_dis";
+            contentClass += "_dis";
+            containerClass += "_dis";
+        }
+
+        boolean fireEffect = false;
+
+
+        String baseID = uiComponent.getClientId(facesContext);
+        if( !state.isFirstTime() &&
+            !state.isChangedViaDecode() &&
+            state.isLastExpandedValue() != open){
+            fireEffect = true;
+        }
+
 
         DOMContext domContext = DOMContext.attachDOMContext(facesContext, uiComponent);
         if (!domContext.isInitialized()) {
@@ -75,14 +107,44 @@ public class PanelAccordionRenderer extends DomBasicRenderer {
         }
         Element root = (Element) domContext.getRootNode();
         root.setAttribute(HTML.CLASS_ATTR, containerClass);
-        String baseID = uiComponent.getClientId(facesContext);
+
         Element header = domContext.createElement(HTML.DIV_ELEM);
-        Text text = domContext.createTextNode(((PanelAccordion) uiComponent).getLabel());
-        header.appendChild(text);
+        //Text text = domContext.createTextNode(((PanelAccordion) uiComponent).getLabel());
+        //header.appendChild(text);
+
         header.setAttribute(HTML.CLASS_ATTR, headerClass);
-        header.setAttribute(HTML.ONCLICK_ATTR, "Ice.Accordion.fire('" + baseID + "_content');");
+        if(panelAccordion.getToogleOnClick().equals(Boolean.TRUE)){
+            if(!disabled)
+                header.setAttribute(HTML.ONCLICK_ATTR, "Ice.Accordion.fire('" + baseID + "_content');");
+        }
+        String script = "Ice.Accordion.collapse('" + baseID + "_content');";
+        if(panelAccordion.getExpanded().booleanValue()){
+            script = "Ice.Accordion.expand('" + baseID + "_content');";
+        }
+
+        if(!disabled)
+            JavascriptContext.addJavascriptCall(facesContext, script);
+
         header.setAttribute(HTML.ID_ATTR, baseID + "_header");
+
         root.appendChild(header);
+        UIComponent headerFacet = uiComponent.getFacet("header");
+        if(headerFacet != null){
+            try{
+
+            //UIComponent headerComp = (UIComponent)headerFacet.getChildren().get(0);
+            domContext.setCursorParent(header);
+            domContext.streamWrite(facesContext, uiComponent,
+                                   domContext.getRootNode(),header);
+
+            CustomComponentUtils.renderChild(facesContext,headerFacet);
+                //System.err.println("Header Renader Done");
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            log.error("No Header facet found in Panel Accordion");
+        }
         //Text script = domContext.createTextNode(SCRIPT);
         //Element scriptEle = domContext.createElement(SCRIPT);
         //scriptEle.setAttribute(HTML.LANG_ATTR, "javascript");
@@ -94,9 +156,19 @@ public class PanelAccordionRenderer extends DomBasicRenderer {
         root.appendChild(container);
         container.setAttribute(HTML.CLASS_ATTR, contentClass);
 
-        if (!open) {
-            container.setAttribute(HTML.STYLE_ATTR, "display:none;");
+
+        if(fireEffect){
+            if(!state.isLastExpandedValue()){
+                container.setAttribute(HTML.STYLE_ATTR, "display:none;");
+            }
+        }else{
+            if (!open) {
+                container.setAttribute(HTML.STYLE_ATTR, "display:none;");
+            }
         }
+        state.setLastExpandedValue(open);
+        state.setChangedViaDecode(false);
+        state.setFirstTime(false);
         domContext.setCursorParent(container);
 
     }
--- component/src/com/icesoft/faces/component/outputchart/OutputChartRenderer.java
+++ component/src/com/icesoft/faces/component/outputchart/OutputChartRenderer.java
@@ -42,7 +42,7 @@ import org.w3c.dom.Element;
 
 import com.icesoft.faces.component.ext.renderkit.FormRenderer;
 import com.icesoft.faces.context.DOMContext;
-import com.icesoft.faces.renderkit.LocationUtil;
+//import com.icesoft.faces.renderkit.LocationUtil;
 import com.icesoft.faces.renderkit.dom_html_basic.DomBasicRenderer;
 import com.icesoft.faces.renderkit.dom_html_basic.HTML;
 
@@ -78,10 +78,10 @@ public class OutputChartRenderer extends DomBasicRenderer {
         Element image = (Element) domContext.createElement(HTML.IMG_ELEM);
 
         //TODO: Convert to LocationUtil.getResourcePath call?
-        image.setAttribute(HTML.SRC_ATTR, LocationUtil.getAppBase(facesContext)
-                +((OutputChart) uiComponent).getFolder().getName()+"/" +
-                                          ((OutputChart) uiComponent)
-                                                  .getFileName());
+        //image.setAttribute(HTML.SRC_ATTR, LocationUtil.getAppBase(facesContext)
+        //        +((OutputChart) uiComponent).getFolder().getName()+"/" +
+       //                                   ((OutputChart) uiComponent)
+        //                                          .getFileName());
         td.appendChild(image);
         if (outputChart.isClientSideImageMap()) {
             Element map = (Element) domContext.createElement(HTML.MAP_ELEM);
