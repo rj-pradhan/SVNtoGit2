d43847bd77f418f1128f859ab8948addee8e0190
rob.mayhew <rob.mayhew>
2007-04-25 22:53:50 +0000

    ICE-1177, Accordion Panel changes
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk@13647 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ icefaces/component/src/com/icesoft/faces/component/accordion/PanelAccordionState.java
@@ -0,0 +1,56 @@
+package com.icesoft.faces.component.accordion;
+
+import com.icesoft.faces.context.BridgeFacesContext;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import java.util.Map;
+
+public class PanelAccordionState {
+
+
+    public static PanelAccordionState getState(FacesContext context, UIComponent component){
+
+        String id = component.getClientId(context);
+        String viewNumber = ((BridgeFacesContext)context).getViewNumber();
+        
+        String key = id + viewNumber;
+        Map map = context.getExternalContext().getSessionMap();
+        PanelAccordionState state = (PanelAccordionState)map.get(key);
+        if(state == null){
+            state = new PanelAccordionState();
+            map.put(key, state);
+        }
+        return state;
+    }
+
+
+    private boolean changedViaDecode;
+    private boolean lastExpandedValue;
+    private boolean firstTime = true;
+
+
+    public boolean isChangedViaDecode() {
+        return changedViaDecode;
+    }
+
+    public void setChangedViaDecode(boolean changedViaDecode) {
+        this.changedViaDecode = changedViaDecode;
+    }
+
+    public boolean isLastExpandedValue() {
+        return lastExpandedValue;
+    }
+
+    public void setLastExpandedValue(boolean lastExpandedValue) {
+        this.lastExpandedValue = lastExpandedValue;
+    }
+
+    public boolean isFirstTime() {
+        return firstTime;
+    }
+
+    public void setFirstTime(boolean firstTime) {
+        this.firstTime = firstTime;
+    }
+}
