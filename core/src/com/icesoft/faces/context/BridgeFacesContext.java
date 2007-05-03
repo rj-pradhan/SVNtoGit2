7a1735866fecfa1745061078673f934349d39506
adnan.durrani <adnan.durrani>
2007-05-03 21:01:27 +0000

    Fix for http://jira.icefaces.org/browse/ICE-1532 (If ICEfaces running under WebLogic the dom update always the "form" element level)
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk/icefaces@13771 8668f098-c06c-11db-ba21-f49e70c34f74

--- core/src/com/icesoft/faces/context/BridgeFacesContext.java
+++ core/src/com/icesoft/faces/context/BridgeFacesContext.java
@@ -357,7 +357,7 @@ public class BridgeFacesContext extends FacesContext {
         for (int i = 0; i < inputElementsLength; i++) {
             Element inputElement = (Element) inputElements.item(i);
             String id = inputElement.getAttribute("id");
-            if (parameters.containsKey(id)) {
+            if (!"".equals(id) && parameters.containsKey(id)) {
                 String value = ((String[]) parameters.get(id))[0];
                 inputElement.setAttribute("value", value);
             }
@@ -368,7 +368,7 @@ public class BridgeFacesContext extends FacesContext {
         for (int i = 0; i < textareaElementsLength; i++) {
             Element textareaElement = (Element) textareaElements.item(i);
             String id = textareaElement.getAttribute("id");
-            if (parameters.containsKey(id)) {
+            if (!"".equals(id) && parameters.containsKey(id)) {
                 String value = ((String[]) parameters.get(id))[0];
                 textareaElement.getFirstChild()
                         .setNodeValue(value);//set value on the Text node
@@ -380,7 +380,7 @@ public class BridgeFacesContext extends FacesContext {
         for (int i = 0; i < selectElementsLength; i++) {
             Element selectElement = (Element) selectElements.item(i);
             String id = selectElement.getAttribute("id");
-            if (parameters.containsKey(id)) {
+            if (!"".equals(id) && parameters.containsKey(id)) {
                 List values = Arrays.asList((String[]) parameters.get(id));
 
                 NodeList optionElements =
