8643c8072fdced32a37ac6089569f08bdec0ed5e
mark.collette <mark.collette>
2007-05-24 15:38:37 +0000

    ICE-1600 : <ice:inputFile/> does not respect the uploadDirectoryAbsolute context-param in web.xml and new "downloadFolder" attribute not documented
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces/trunk/icefaces@13964 8668f098-c06c-11db-ba21-f49e70c34f74

--- core/src/com/icesoft/faces/webapp/http/servlet/FileUploadComponent.java
+++ core/src/com/icesoft/faces/webapp/http/servlet/FileUploadComponent.java
@@ -3,12 +3,13 @@ package com.icesoft.faces.webapp.http.servlet;
 import com.icesoft.faces.context.BridgeFacesContext;
 import org.apache.commons.fileupload.FileItemStream;
 
+import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.io.Writer;
 
 public interface FileUploadComponent {
     
-    void upload(FileItemStream fileItemStream, String defaultFolder, long maxSize, BridgeFacesContext bfg) throws IOException;
+    void upload(FileItemStream fileItemStream, String uploadDirectory, boolean uploadDirectoryAbsolute, long maxSize, BridgeFacesContext bfg, ServletContext servletContext, String sessionId) throws IOException;
 
     void setProgress(int percentage);
 
